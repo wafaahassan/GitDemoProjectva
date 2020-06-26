abed42f7eb1d094b2a513b2bc22f1ec58f729e80
wafa <wafa@DESKTOP-G590DST>
2020-06-26 07:06:30 -0400

    First commit.....

---
+++ src/defaultpackage/Test1.java
@@ -0,0 +1,15 @@
+package defaultpackage;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+	 String hungery= "yes"	;
+	 if (hungery=="yes")
+		 System.out.println("eat somthing....");
+	 else {
+		 System.out.println("Do work.......");
+	 }
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module GitDemoProjectv1 {
+}
