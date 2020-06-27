db2e8edab155db9b39169e88e1b9a9a85f34cba5
wafa <wafa@DESKTOP-G590DST>
2020-06-27 12:10:49 -0400

    i want to edit my w

---
+++ src/defaultpackage/pactice.java
@@ -0,0 +1,15 @@
+package defaultpackage;
+
+public class pactice {
+
+	public static void main(String[] args) {
+		System.out.println("my mane is w");
+		System.out.println("learning code");
+		System.out.println("wafa 5");
+		
+		
+		System.out.println("change in jc loginpage ve");
+
+	}
+
+}
